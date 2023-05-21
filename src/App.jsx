225f83c65669647599207e463a8b9a79f51e1aac
Erison <erison.santos@ymail.com>
2023-05-20 23:18:22 -0400

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import { Header } from './components/Header'
-import { Post } from './components/Post'
-import { Sidebar } from './components/Sidebar'
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
-import styles from './App.module.css'
-import './global.css'
+import styles from "./App.module.css";
+import "./global.css";
 
 export function App() {
-
   return (
     <div>
       <Header />
-    <div className={styles.wrapper}>
-      <Sidebar />
-      <main>
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam doloremque nesciunt modi error autem nostrum rerum illum, tempora, nemo sit assumenda ea fugiat quod voluptate suscipit! Fuga totam ipsa vitae."
-      />
-      <Post 
-        author="Gabriel Pedro"
-        content="Novo poste muito legal!"
-      />
-      </main>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam doloremque nesciunt modi error autem nostrum rerum illum, tempora, nemo sit assumenda ea fugiat quod voluptate suscipit! Fuga totam ipsa vitae."
+          />
+          <Post author="Gabriel Pedro" content="Novo poste muito legal!" />
+        </main>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return (
-        <aside className={styles.Sidebar}>
+        <aside className={styles.sidebar}>
             <img
                 className={styles.cover}
                 src="https://images.unsplash.com/photo-1590845947670-c009801ffa74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80"
